b9b2d1fa5d899309d7826f5f965a6c266ec1540f
June Kim <june6723@naver.com>
2020-11-09 10:01:01 +0900

    Updates

---
+++ precache-manifest.d91de3b6ee75ab2910fe4fabcc624e74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a00849c37678101fc4ee48594ac89f12",
+    "url": "/jetflix/index.html"
+  },
+  {
+    "revision": "a5883a62e0863a49d2db",
+    "url": "/jetflix/static/js/2.41244f17.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/jetflix/static/js/2.41244f17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "460fe549540cc0a85a0c",
+    "url": "/jetflix/static/js/main.3bff56a8.chunk.js"
+  },
+  {
+    "revision": "6a6e7b31d7a10a1427d9",
+    "url": "/jetflix/static/js/runtime-main.6dba2a84.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/jetflix/static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
